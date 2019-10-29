094780f465659e907c19b0bb6594d5b85896de2e
Jorge Guimarães <jorgeguimaraes@cinbesa.com.br>
2019-10-29 17:35:52 -0300

    add sequelize, pg, phstore, slint, prettier

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-import express from 'express';
-import routes from './routes'
+import express from "express";
+import routes from './routes';
 
 class App {
-    constructor(){
+    constructor() {
         this.server = express();
-        
+
         this.middleware();
         this.routes();
     }
 
-
-    middleware(){
+    middleware() {
         this.server.use(express.json());
     }
 
-    routes(){
+    routes() {
         this.server.use(routes);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
-
 const routes = new Router();
 
-
-routes.get('/', (req, res)=>{
-    return res.json({msg:"agora tudo dividido"});
-})
+routes.get('/', (req, res) => {
+  return res.json({ msg: 'agora tudo dividido' });
+});
 
 export default routes;
